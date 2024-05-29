fcdb18716127be0766be54600003807857e72e97
Jesse MacFadyen <purplecabbage@gmail.com>
2024-05-29 15:53:11 -0700

    less code, less problems

--- src/lib/run-dev.js
+++ src/lib/run-dev.js
@@ -378,7 +378,7 @@ async function invokeAction ({ actionRequestContext, logger }) {
       statusCode = statusCode || 200 // this is the OW default if omitted
       body = body || ''
       const isError = statusCode >= 400
-      const isObject = isObjectNotArray(response)
+      const isObject = typeof response === 'object' && !Array.isArray(response)
 
       return {
         ...(isObject && !isError ? response : {}), // pass all the other properties as well if an object, and not an error
@@ -519,16 +519,6 @@ function createActionParametersFromRequest ({ req, actionInputs = {} }) {
   }
 }
 
-/**
- * Returns true if the item is an object (but not an Array)
- *
- * @param {*} item the item to test
- * @returns {boolean} true if the item is an object (but not an Array)
- */
-function isObjectNotArray (item) {
-  return typeof item === 'object' && !Array.isArray(item)
-}
-
 module.exports = {
   runDev,
   serveWebAction,
@@ -539,6 +529,5 @@ module.exports = {
   statusCodeMessage,
   isRawWebAction,
   isWebAction,
-  createActionParametersFromRequest,
-  isObjectNotArray
+  createActionParametersFromRequest
 }
